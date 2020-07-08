02c11c2700fc964c69c35b93c15109b81aabd265
William Davis <liamdavis@tuta.io>
2020-07-08 02:00:30 -0400

    Created matched profile card using Card component from Material UI. Started styling.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import MatchedProfileCard from "./components/UI/MatchedProfileCard";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <MatchedProfileCard />
     </div>
   );
 }
